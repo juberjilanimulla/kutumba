f25ca1114f0240a2bf42c57afde00eac0ca851c2
Juber_Mulla <juberjilanimulla@gmail.com>
2025-09-26 11:52:44 +0530

    this is commit for event routers

--- routes/userRouter/userfunctionRouter.js
+++ routes/userRouter/userfunctionRouter.js
@@ -1,5 +1,20 @@
 import { Router } from "express";
+import functionmodel from "../../models/functionmodel.js";
+import {
+  successResponse,
+  errorResponse,
+} from "../../helpers/serverResponse.js";
 
 const userfunctionRouter = Router();
 
+userfunctionRouter.create("/post", createfunctionHandler);
+
 export default userfunctionRouter;
+
+async function createfunctionHandler(req, res) {
+  try {
+  } catch (error) {
+    console.log("error", error);
+    errorResponse(res, 500, "internal server error");
+  }
+}
