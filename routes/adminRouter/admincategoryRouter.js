6838c823423cf9c2553697e33bd12208d826b696
Juber_Mulla <juberjilanimulla@gmail.com>
2025-12-01 17:25:37 +0530

    this is commit for models

--- routes/adminRouter/admincategoryRouter.js
+++ routes/adminRouter/admincategoryRouter.js
@@ -1,5 +1,80 @@
 import { Router } from "express";
+import categorymodel from "../../models/categorymodel.js";
+import {
+  successResponse,
+  errorResponse,
+} from "../../helpers/serverResponse.js";
 
 const admincategoryRouter = Router();
 
+admincategoryRouter.get("/", getallcategoryHandler);
+admincategoryRouter.post("/create", createcategoryHandler);
+admincategoryRouter.put("/update", updatecategoryHandler);
+admincategoryRouter.delete("/delete", deletecategoryHandler);
+
 export default admincategoryRouter;
+
+async function getallcategoryHandler(req, res) {
+  try {
+    const categories = await categorymodel.find();
+    successResponse(res, "success", categories);
+  } catch (error) {
+    console.log("error", error);
+    errorResponse(res, 500, "internal server error");
+  }
+}
+
+async function createcategoryHandler(req, res) {
+  try {
+    const { name } = req.body;
+    if (!name) {
+      return errorResponse(res, 400, "some params are missing");
+    }
+    const existingcategory = await categorymodel.findOne({ name });
+    if (existingcategory) {
+      return errorResponse(res, 400, "category already exists");
+    }
+    const category = await categorymodel.create({ name });
+    successResponse(res, "success", category);
+  } catch (error) {
+    console.log("error", error);
+    errorResponse(res, 500, "internal server error");
+  }
+}
+
+async function updatecategoryHandler(req, res) {
+  try {
+    const { categoryid, name } = req.body;
+    if (!categoryid || !name) {
+      return errorResponse(res, 400, "some params are missing");
+    }
+    const existingcategory = await categorymodel.findById({ _id: categoryid });
+    if (!existingcategory) {
+      return errorResponse(res, 404, "category not found");
+    }
+    existingcategory.name = name;
+    await existingcategory.save();
+    successResponse(res, "success", existingcategory);
+  } catch (error) {
+    console.log("error", error);
+    errorResponse(res, 500, "internal server error");
+  }
+}
+
+async function deletecategoryHandler(req, res) {
+  try {
+    const { _id } = req.body;
+    if (!_id) {
+      return errorResponse(res, 400, "some params are missing");
+    }
+    const existingcategory = await categorymodel.findById({ _id });
+    if (!existingcategory) {
+      return errorResponse(res, 404, "category not found");
+    }
+    await categorymodel.findByIdAndDelete({ _id });
+    successResponse(res, "success");
+  } catch (error) {
+    console.log("error", error);
+    errorResponse(res, 500, "internal server error");
+  }
+}
--- routes/adminRouter/adminsubcategoryRouter.js
+++ routes/adminRouter/adminsubcategoryRouter.js
@@ -1,5 +1,78 @@
 import { Router } from "express";
+import subcategorymodel from "../../models/subcategorymodel.js";
+import {
+  successResponse,
+  errorResponse,
+} from "../../helpers/serverResponse.js";
 
 const adminsubcategoryRouter = Router();
+adminsubcategoryRouter.get("/", getallsubcategoryHandler);
+adminsubcategoryRouter.post("/create", createsubcategoryHandler);
+adminsubcategoryRouter.put("/update", updatesubcategoryHandler);
+adminsubcategoryRouter.delete("/delete", deletesubcategoryHandler);
 
 export default adminsubcategoryRouter;
+
+async function getallsubcategoryHandler(req, res) {
+  try {
+    const subcategories = await subcategorymodel.find();
+    successResponse(res, "success", subcategories);
+  } catch (error) {
+    console.log("error", error);
+    errorResponse(res, 500, "internal server error");
+  }
+}
+async function createsubcategoryHandler(req, res) {
+  try {
+    const { categoryid, name } = req.body;
+    if (!categoryid || !name) {
+      return errorResponse(res, 400, "some params are missing");
+    }
+    const existingsubcategory = await subcategorymodel.findOne({ name });
+    if (existingsubcategory) {
+      return errorResponse(res, 400, "subcategory already exists");
+    }
+    const params = { categoryid, name };
+    const subcategory = await subcategorymodel.create(params);
+    successResponse(res, "success", subcategory);
+  } catch (error) {
+    console.log("error", error);
+    errorResponse(res, 500, "internal server error");
+  }
+}
+
+async function updatesubcategoryHandler(req, res) {
+  try {
+    const { _id, name } = req.body;
+    if (!_id || !name) {
+      return errorResponse(res, 400, "some params are missing");
+    }
+    const existingsubcategory = await subcategorymodel.findById({ _id });
+    if (!existingsubcategory) {
+      return errorResponse(res, 404, "subcategory not found");
+    }
+    existingsubcategory.name = name;
+    await existingsubcategory.save();
+    return successResponse(res, "success", existingsubcategory);
+  } catch (error) {
+    console.log("error", error);
+    errorResponse(res, 500, "internal server error");
+  }
+}
+
+async function deletesubcategoryHandler(req, res) {
+  try {
+    const { _id } = req.body;
+    if (!_id) {
+      return errorResponse(res, 400, "some params are missing");
+    }
+    const existingsubcategory = await subcategorymodel.findById({ _id });
+    if (!existingsubcategory) {
+      return errorResponse(res, 404, "subcategory not found");
+    }
+    await subcategorymodel.deleteOne({ _id });
+    successResponse(res, "success");
+  } catch (error) {
+    console.log("error", error);
+  }
+}
